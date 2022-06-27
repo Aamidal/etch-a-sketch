e903d9abb3806bb9bca92716bb8196c2e71a8413
Jack Kellenberger <jackson@kellenberger.io>
2022-06-26 20:37:06 -0400

    Refactor button functions to reduce repeated code

--- sketch.js
+++ sketch.js
@@ -7,14 +7,9 @@ const rainbowBtn = document.getElementById('rainbow');
 const eraserBtn = document.getElementById('eraser');
 const picker = document.getElementById('picker');
 const brushBtn = document.getElementById('brush');
-
-let static = true;
-let random = false;
-let eraser = false;
+let brushType = 'static';
 let brush = false;
-
 let color = '#2aa198'
-sliderLabel.innerHTML = `Grid Size: ${slider.value} x ${slider.value}`
 
 function sizeGrid(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
@@ -28,8 +23,8 @@ function sizeGrid(size) {
 
 function changeColor(e) {
     let pix = e.target;
-    if (random) pix.style.backgroundColor = randomColor();
-    else if (eraser) pix.style.backgroundColor = '#fdf6e3'
+    if (brushType == 'random') pix.style.backgroundColor = randomColor();
+    else if (brushType == 'eraser') pix.style.backgroundColor = '#fdf6e3'
     else pix.style.backgroundColor = picker.value;
 }
 
@@ -42,41 +37,25 @@ function updateSize() {
 function updateColor(e) {
     color = e.target.value;
     picker.style.backgroundColor = color;
-    setStatic();
+    setBrush();
 }
 
 function randomColor() {
     solarized = ['#859900', '#2aa198', '#268bd2', '#6c71c4', '#d33682', 
             '#dc322f', '#cb4b16', '#b58900']
-    console.log(color)
     return color = solarized[Math.floor(Math.random()*solarized.length)]
 }
 
-function setStatic () {
-    eraserBtn.className = '';
-    rainbowBtn.className = '';
-    staticBtn.className ='picked';
-    static = true;
-    random = false;
-    eraser = false;
+function setBrush (brushBtn=staticBtn, type='static') {
+    clearBtns();
+    brushBtn.className = 'picked';
+    brushType = type;
 }
 
-function setRandom () {
+function clearBtns() {
     eraserBtn.className = '';
-    rainbowBtn.className = 'picked';
-    staticBtn.className ='';
-    static = false;
-    random = true;
-    eraser = false;
-}
-
-function setEraser () {
-    eraserBtn.className = 'picked';
+    staticBtn.className = '';
     rainbowBtn.className = '';
-    staticBtn.className ='';
-    eraser = true;
-    random = false;
-    static = false;
 }
 
 function toggleBrush () {
@@ -101,19 +80,15 @@ function toggleBrush () {
     }
 }
 
-function startUp() {
-    setStatic();
-    sizeGrid(slider.value);
-    picker.value = color;
-    picker.style.backgroundColor = color;
-    picker.addEventListener("input", updateColor, false);
-    picker.select();
-    slider.addEventListener('input', updateSize);
-    reset.addEventListener('click', updateSize);
-    staticBtn.addEventListener('click', setStatic)
-    eraserBtn.addEventListener('click', setEraser)
-    rainbowBtn.addEventListener('click', setRandom)
-    grid.addEventListener('click', toggleBrush)
-}
-
-startUp();
+setBrush();
+updateSize();
+picker.value = color;
+picker.style.backgroundColor = color;
+picker.addEventListener("input", updateColor, false);
+picker.select();
+slider.addEventListener('input', updateSize);
+reset.addEventListener('click', updateSize);
+staticBtn.addEventListener('click', ()=>setBrush(staticBtn, 'static'))
+eraserBtn.addEventListener('click', ()=> setBrush(eraserBtn, 'eraser'))
+rainbowBtn.addEventListener('click', ()=>setBrush(rainbowBtn, 'random'))
+grid.addEventListener('click', toggleBrush)
